d5b4ce97fdc5957271eaa4cda5186730dca1c746
siawaseok3 <siawaseok@outlook.jp>
2024-10-22 01:32:36 +0900

    Update dino-game.js

--- anime/dino-game.js
+++ anime/dino-game.js
@@ -10,11 +10,11 @@ const dinoImages = [
     new Image(),
     new Image()
 ];
-dinoImages[0].src = '/photo1/'; // 恐竜の最初の画像
-dinoImages[1].src = '/photo2/'; // 恐竜の2つ目の画像
+dinoImages[0].src = '/photo1/photo1.png'; // 恐竜の最初の画像
+dinoImages[1].src = '/photo2/photo2.png'; // 恐竜の2つ目の画像
 
 const obstacleImage = new Image();
-obstacleImage.src = '/photo3/'; // 障害物の画像
+obstacleImage.src = '/photo3/photo3.png'; // 障害物の画像
 
 let currentImageIndex = 0;
 let frameCount = 0;
